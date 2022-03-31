ea089c044cf164c5f9083c23cb4c882236eb6814
Felipe Lima <felipe_e_lima@estudante.sesisenai.org.br>
2022-03-31 09:23:24 -0300

    adicionando codigos de logica

---
+++ logica_computacional/código numeros primos.java
@@ -0,0 +1,30 @@
+codigo para listar os numeros primos: 
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner dape = new Scanner(System.in);
+        
+        System.out.print("Digite o número: ");
+        int n = dape.nextInt();
+        int i = 0;
+        
+            while(i <= n){
+                int contador = 0;
+                int j = i;
+                
+            while(j >= 1){
+                if(i % j == 0){
+                    
+                contador++;}
+        
+                j--;}
+            
+        if(contador == 2){
+        System.out.println(i);}
+    
+        i++;
+        
+        }
+    }
+}
