0c1c7150aea174456e05e6f89fea3253f73fc5af
Felipe Eduardo Gomes Souto Maior de Oliveira Lima <102593407+FelipeEduardo16@users.noreply.github.com>
2022-06-30 09:15:34 -0300

    Update código.ino

--- fundamentos_de_TI/autorias/tela_lcd/código.ino
+++ fundamentos_de_TI/autorias/tela_lcd/código.ino
@@ -1,1 +1,18 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27,16,2);
+ 
+void setup()
+{
+ lcd.init();
+}
+ 
+void loop()
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("O vitin");
+  lcd.setCursor(0,1);
+  lcd.print("é um gato");
+}
